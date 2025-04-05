594ddf76d939fd438d9c5c457c6313d7f1e1dbc6
wjtls <wjtls01@naver.com>
2025-04-05 21:25:25 +0900

    .

--- main/views_oversea_stock.py
+++ main/views_oversea_stock.py
@@ -17,111 +17,34 @@ import numpy as np
 # 정규화 및 전처리 계산
 import requests
 import glob
+import json
 
 
 
 # Create your views here.
 
+def get_file_path(symbol):
+    # 현재 실행 중인 파일의 절대 경로
+    current_file_path = os.path.abspath(__file__)
+
+    # 현재 파일의 디렉토리 경로
+    current_dir = os.path.dirname(current_file_path)
+
+    # 목표 파일 상대 경로
+    relative_path = f'../../a_FRDdata_api/price_real_data/real_data_{symbol}_1m.json'
+
+    # 목표 파일 절대 경로 계산
+    absolute_path = os.path.abspath(os.path.join(current_dir, relative_path))
+
+    return absolute_path
 
 def oversea_api(request, minute, symbol, exchange_code):
-    # 한투API 실시간 호출
-    now = datetime.datetime.now()
-    now = now.strftime("%Y-%m-%d %H:%M")
-    data_count = ["2010-01-01 00:00", now]
-    ACCESS_TOKEN , token_time= get_ovsstk_chart_price.get_access_token()
-    print(ACCESS_TOKEN, 'access token')
-    print(exchange_code,symbol,'info')
-    # 실시간 데이터 호출
-    real_time_data = get_ovsstk_chart_price.fetch_and_save_data(exchange_code, symbol, 1, 40, ACCESS_TOKEN) #거래소코드,심볼
-
-    # 현재 실행 스크립트의 디렉토리 경로
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-
-    # 현재 디렉토리에서 상위 디렉토리로 이동한 후 a_FRDdata_api 폴더로 이동
-    base_path = os.path.join(current_dir, "..","..", "a_FRDdata_api")
-
-    # 파일 이름
-    file_name_pattern = f"{symbol}_full_1min_adjsplitdiv.txt"
-
-    # glob을 사용하여 해당 경로에 있는 모든파일에서 찾음
-    print(f'찾으려는 경로,파일 명 : {base_path}/{file_name_pattern}')
-    search_pattern = os.path.join(base_path, '**', file_name_pattern)
-    files = glob.glob(search_pattern, recursive=True)
-
-    if files:
-        past_data_name = files[0]  # 첫 번째로 찾은 파일 사용
-        past_data = pd.read_csv(past_data_name)
-        past_data.columns = ['datetime', 'open', 'high', 'low', 'last', 'evol']
-        print(f"FRD 데이터 파일 서치완료: {past_data_name}")
-    else:
-        print(f"저장된 FRD 데이터 전체에서 파일을 찾을 수 없습니다.   찾은 파일 : {past_data_name}")
-        past_data_name = files  # 첫 번째로 찾은 파일 사용
-        past_data =pd.read_csv(past_data_name)
-
-
-    # 형식 변환
-    past_data['datetime'] = pd.to_datetime(past_data['datetime'])
-    real_time_data['datetime'] = pd.to_datetime(real_time_data['datetime'])
-
-    # 분봉 변환
-
-    # datetime 열을 인덱스로 설정
-    past_data.set_index('datetime', inplace=True)
-    real_time_data.set_index('datetime', inplace=True)
-
-    # 시간 필터
-    past_data = past_data.between_time('09:00', '16:00')
-    real_time_data = real_time_data.between_time('09:00', '16:00')
-
-    print(past_data,'이전 데이터', minute, '설정 분봉', symbol, '심볼')
-    print(real_time_data, '실시간 데이터', minute, '설정 분봉', symbol, '심볼')
-    # 합침
-    combined_data = pd.concat([past_data, real_time_data], axis=0)
-    combined_data.reset_index(inplace=True)
-    combined_data = combined_data.drop_duplicates(subset='datetime')
-    combined_data.columns = ['Datetime', 'open', 'high', 'low', 'last','volume', 'eamt']
-
-    # datetime 형식으로 변환
-    start_date = pd.to_datetime(data_count[0])
-    end_date = pd.to_datetime(data_count[1])
-
-    # 최종 데이터 추출
-    filtered_data = combined_data[
-        (combined_data['Datetime'] >= start_date) & (combined_data['Datetime'] <= end_date)]
-
-    df = filtered_data[['Datetime', 'open', 'high', 'low', 'last', 'volume']]
-    data_set = pd.DataFrame(df).reset_index()
-    #data_set = total_time_Frame(data_set.values.tolist(), minute, data_set.columns)
-
-    # 각 열을 Series로 변환 및 NaN 처리
-    open_ = pd.to_numeric(data_set['open'], errors='coerce').fillna(0).astype(float).tolist()
-    close_ = pd.to_numeric(data_set['last'], errors='coerce').fillna(0).astype(float).tolist()
-    high_ = pd.to_numeric(data_set['high'], errors='coerce').fillna(0).astype(float).tolist()
-    low_ = pd.to_numeric(data_set['low'], errors='coerce').fillna(0).astype(float).tolist()
-    vol = pd.to_numeric(data_set['volume'], errors='coerce').fillna(0).astype(float).tolist()
-    date = pd.to_datetime(data_set['Datetime'])  # Datetime 형식으로 변환
-
-    # 데이터프레임을 사용하여 데이터 생성
-    data = {
-        'x': date,
-        'o': open_,
-        'h': high_,
-        'l': low_,
-        'c': close_
-    }
-
-    # 각 열을 리스트로 변환
-    data_list = []
-    for i in range(len(data['x'])):
-        data_list.append({
-            'localDate': pd.to_datetime(data['x'].iloc[i]),  # ISO 형식으로 변환
-            'openPrice': float(data['o'][i]),
-            'highPrice': float(data['h'][i]),
-            'lowPrice': float(data['l'][i]),  #
-            'closePrice': float(data['c'][i])  #
-        })
-    data_list =data_list[-10000:]
-    print('데이터 개수 :',len(data_list))
+    path = get_file_path(symbol)
+    print('불러오기')
+    with open(path, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    data_res = data.get('response')  # 로드된 데이터 출력
+    data_list = data_res['data']
     return JsonResponse({'status': True, 'response': {'data': data_list}}, status=200)
 
 
